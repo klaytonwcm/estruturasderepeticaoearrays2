1aee32228f42fc02f31ca5426b409398720e0e37
klaytonwcm <klayton.montenegro@gmail.com>
2021-12-23 19:31:00 -0400

    Exercise 7 Arrays commited

---
+++ src/com/dio/estrutura/exercicio7.java
@@ -0,0 +1,14 @@
+package com.dio.estrutura;
+
+public class exercicio7 {
+    public static void main(String[] args) {
+
+        int[] vetor = {-7,-5,17,34,3};
+
+        System.out.print("Vetor: ");
+
+        for (int i = vetor.length -1; i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
