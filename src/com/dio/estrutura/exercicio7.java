68a01e0af60fcd1579c94ce8d9f588de8c5afc04
klaytonwcm <klayton.montenegro@gmail.com>
2021-12-23 19:33:02 -0400

    Exercise 7 Arrays commited

--- src/com/dio/estrutura/exercicio7.java
+++ src/com/dio/estrutura/exercicio7.java
@@ -3,10 +3,12 @@ package com.dio.estrutura;
 public class exercicio7 {
     public static void main(String[] args) {
 
+        //Vetor com 5 elementos
         int[] vetor = {-7,-5,17,34,3};
 
         System.out.print("Vetor: ");
 
+        //Repetição Inversa com -- para exibir vetor Invertido
         for (int i = vetor.length -1; i >= 0; i--) {
             System.out.print(vetor[i] + " ");
         }
